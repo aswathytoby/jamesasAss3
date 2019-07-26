91019f81989902d9520f31ac4e09fc874b92b603
aswathytoby <52545018+aswathytoby@users.noreply.github.com>
2019-07-25 22:58:38 -0400

    Add files via upload

---
+++ assets/Data/mySongs.ts
@@ -0,0 +1,36 @@
+import { SongInfo } from "../../app/songInfo";
+
+export const MYSONGS: SongInfo[] = [
+  {
+    songname: "We are family",
+    
+    artist: " Noel Harrison",
+    genre: "Disco",
+    year: "1979",
+    pic:'./assets/images/weare family.jpg'
+  },
+  {
+    songname: "sorry",
+    artist: " Justin Bieber",
+    genre: "Dancehall-pop",
+    year: "2015",
+    pic:'./assets/images/sorry.png'
+    
+  },
+  {
+    songname: "Death of Titanic",
+    artist: " James Horner",
+    genre: " pop",
+    year: "1997",
+    pic:'./assets/images/titanic.jpg'
+    
+  },
+  {
+    songname: "Karutha Pennne",
+    artist: " K.S Chithra",
+    genre: " pop",
+    year: "1994",
+    pic:'./assets/images/karuthapenne.jpg'
+    
+  }
+];
